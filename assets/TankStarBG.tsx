cf354efa236b3df12eb363adbc286dff40f50890
VASAN VOHRA <vasanvohra@VASANs-MacBook-Air.local>
2023-07-08 21:07:23 +0530

    Project

---
+++ assets/TankStarBG.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="TankStarBG" tilewidth="1161" tileheight="536" tilecount="1" columns="1">
+ <image source="/Users/varul18/Desktop/Game/assets/TankStarBG.png" width="1161" height="536"/>
+</tileset>
